aefa58ec71848e790af4e03ba4318b9e5f8a3641
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-11-12 13:29:29 -0800

    update

--- PythonAbstraction.py
+++ PythonAbstraction.py
@@ -1,28 +1,21 @@
 from abc import ABC, abstractmethod 
 class Computer(ABC):
+    def memory(self, amount):
+        print("memory amount:", amount)
+        
 
     @abstractmethod
     def process(self):
         pass
+        
 
 class Laptop(Computer):
-    def process(self):
-        print("its running")
-        
-class Whiteboard(Computer):
-    def write(self):
-        print("its writing")
-        
-        
+    def process(self, amount):
+        print('your memory amount {} exceeds your 100% limit'.format(amount))
 
-obj = Whiteboard()
-obj.process("writing")
-obj.Laptop("running")
-
-
-
-
-
-
+obj = Laptop()
+obj.memory("120%")
+obj.process("120%")
+    
 
 
